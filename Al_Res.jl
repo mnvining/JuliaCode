0f05234ae08b7f5c55bf2602155285408a91dd9b
Melanie N Vining (J.Gibson 9/2014) <mnv2001@monroe.unh.edu>
2018-04-19 09:49:21 -0400

    Res1 important

--- Al_Res.jl
+++ Al_Res.jl
@@ -2,11 +2,12 @@ include("FuncOut.jl")
 include("TryinBig.jl")
 mm=121
 Al=logspace(-4,2,mm)
-z=[]
+z=zeros(1,2)
 for i=1:mm
     (acc,acc2,stab,d)=RUN2(Int(32),BigFloat(Al[i]),Int(9),Int(6),Int(5),Int(8),0,1e5,1)
     if stab <=1
-        z=vcat(z,hcat(Al[i],d))
+    L=hcat(Al[i],d)
+        z=vcat(z,L)
     end
     
 end
