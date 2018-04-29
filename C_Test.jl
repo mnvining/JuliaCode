d4ae680f9abb3de3cba6cba409aa11484641e879
Melanie N Vining (J.Gibson 9/2014) <mnv2001@monroe.unh.edu>
2018-04-29 15:38:22 -0400

    This is the C_Test you want to use

--- C_Test.jl
+++ C_Test.jl
@@ -8,9 +8,9 @@ include("coeffcalc.jl")
 include("hsol.jl")
 include("ConvertToBig.jl")
 include("GreensInt.jl")
-mm=41 # number of values
-Al=logspace(-3.5,-2,mm)
-C_Guess=logspace(-20,-10,mm)
+mm=61 # number of values
+Al=logspace(-4,2,mm)
+C_Guess=logspace(1,7,mm)
 cl=Int(5)
 
 acc=zeros(mm,mm)
@@ -23,13 +23,17 @@ stab=zeros(mm,mm)
 # Note: choice of acc/stab means can be varied from poly to poly
 # Note: value of c_1,c_2 can be varied from poly AND Alpha
 
-Z=[];
+Z=zeros(1,3)
 for i=1:mm
     for j=1:mm
         #RUN2(NG,Al,n,d,cl,opt,plotopt,c_1,c_2)
-        (acc[i,j],acc2,stab[i,j],d)=RUN2(Int(32),BigFloat(Al[i]),Int(9),Int(6),Int(5),Int(8),0,1/(C_Guess[j]),1)
-    end
-    println(i)
+        (acc[i,j],acc2,stab[i,j],d)=RUN2(Int(32),BigFloat(Al[i]),Int(9),Int(6),Int(5),Int(8),0,(C_Guess[j],1)
+if stab[i,j]<=1
+La=hcat(Al[i],C_Guess[j],stab[i,j])
+Z=vcat(Z,La)
+end
+end
+    
 end
 
 
