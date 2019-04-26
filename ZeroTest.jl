b07c138ece7d6a253fd62feaace90ec03fbbbe1c
Melanie Vining <melanie.n.vining@gmail.com>
2019-04-26 13:50:58 -0400

    Added two endpoints to the cosine xfm mtx. Need to do the same to sin. updated zero test.

--- ZeroTest.jl
+++ ZeroTest.jl
@@ -3,10 +3,10 @@ using LinearAlgebra
 using PyPlot
 using GenericSVD
 using Roots
-function MinProb(d,Al,Lam,Mu,Gam)
+function ZeroTest(d,Al,Lam,Mu,Gam)
 
 
-    include("CosMtx.jl")
+    include("CosMtx2.jl")
     include("CosDer.jl")
     include("CosDer2.jl")
     include("All.jl")
@@ -42,20 +42,7 @@ function MinProb(d,Al,Lam,Mu,Gam)
     xx=linspace(BigFloat(-1),BigFloat(1),Int(F*(n-1)+1));
     y=zeros(BigFloat,size(xx))
     y=evalasarray(f(x),xx)
-
-    GG(x)=TotalPoly(d,n,Al);
-    EGGf=evalasarray(GG(x),xx);
-    (h1,h2)=hsol(xx,Al,1)
-    Eh1f=(evalasarray(h1(x),xx))
-    Eh2f=(evalasarray(h2(x),xx))
-    println(norm(EGGf[1:10:end]))
-    if norm(EGGf[1:10:end])<=1
-        c1=0
-    else
-        T(CC)=norm(EGGf[1:10:end]-CC*Eh1f[1:10:end]-CC*Eh2f[1:10:end])-(1-Al/1000);
-        c1=find_zero(T,Al)
-    end
-    println(c1)
+    ya=vcat(1,0*y,1)
 
     #if d==0
     #c1=-1.409815245715622e-05
@@ -73,12 +60,14 @@ function MinProb(d,Al,Lam,Mu,Gam)
     #-1.958059513322267
     #end
 
-    BF=EGGf-c1*Eh1f-c1*Eh2f;
+    (M,B,C)=CosMtx2(D,A,n,F);
 
-    (M,B,C)=CosMtx(D,A,n,F);
+    (U,S,V)=svd(B);
+    S=Diagonal{BigFloat}(S)
+    Sdag=pinv(S,1e-40);
+    Res=V*(Sdag*(U'*ya));
 
-    (s1,s2)=size(B);
-
+    CF=M*Res;
 
     (D1,D2)=CosDer2(D,A,n,F);
 
@@ -87,27 +76,13 @@ function MinProb(d,Al,Lam,Mu,Gam)
     for i=1:length(DO[1,:])
         IDO[i,i]=1/DO[i,i];
     end
-    Mat1=B*IDO;
-    r=setdiff(1:s1,1:10:s1);
-    Mat1=Mat1[setdiff(1:end,r),:]
-    Ent_1=hcat(B,zeros(BigFloat,s1,2));
-    Ent_2=hcat(Mu*Mat1,Mu*Al*Eh1f[1:10:end],Mu*Al*Eh2f[1:10:end]);
-    Ent_3=hcat(zeros(BigFloat,s1,s2),Lam*Eh1f,Lam*Eh2f)
-    Ent_4=hcat(Gam*B,zeros(BigFloat,s1,2));
+    CU=M*IDO*Res;
+    UG=CU[428:518]
 
-    MM=vcat(Ent_1,Ent_2,Ent_3,Ent_4);
-    RHS=vcat(y,Mu*BF[1:10:end],0*Eh1f,0*y)
-    (Um,Sm,Vm)=svd(MM)
-    Sm=Diagonal{BigFloat}(Sm);
-    SmD=pinv(Sm,1e-40)
-    Res=Vm*(SmD*(Um'*(RHS)));
-    LL=Int(428)
-    fc=M*Res[1:end-2];
-    f_AD=fc[LL:LL+90];
-    uc=M*(IDO*Res[1:end-2])
-    u_AD=uc[LL:LL+90]
+    FG=CF[428:518]
 
-    return norm(f_AD-y),norm(u_AD[1:10:end])/norm(y[1:10:end]),Res[end-1:end],fc,uc,BF[1:10:end],y,EGGf
+
+    return CF,norm(FG.-0*y),norm(UG[1:10:end])/norm(y[1:10:end]),Coarse
 
 
 
