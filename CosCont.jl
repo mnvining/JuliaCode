1ab95a6e1274699a82c85d9d6b5a3ff5ba8d6154
Melanie Vining <melanie.n.vining@gmail.com>
2019-04-28 21:15:30 -0400

    CosCont now spits out f_c for use in SVD prob

--- CosCont.jl
+++ CosCont.jl
@@ -16,9 +16,9 @@ function CosCont(d,Al,Lam)
     include("DiffOp.jl")
     include("GreensInt.jl")
     include("myhouse.jl")
-    
+
     x=Sym("x")
-    
+
 
     setprecision(200);
     D=BigFloat(350)/BigFloat(100);
@@ -43,13 +43,13 @@ function CosCont(d,Al,Lam)
     EGGf=evalasarray(GG(x),xx);
     (h1,h2)=hsol(xx,Al,0)
     Eh1f=(evalasarray(h1(x),xx))
-    Eh2f=(evalasarray(h2(x),xx)) 
+    Eh2f=(evalasarray(h2(x),xx))
 
     (M,B,C)=CosMtx(D,A,n,F);
-    
+
     (s1,s2)=size(B);
 
-    
+
     (D1,D2)=CosDer2(D,A,n,F);
 
     DO=DiffOp(D,A,n,F,Al,1);
@@ -57,12 +57,12 @@ function CosCont(d,Al,Lam)
     for i=1:length(DO[1,:])
         IDO[i,i]=1/DO[i,i];
     end
-    
-    
+
+
     Ent_1=hcat(B,zeros(BigFloat,s1,2));
     Ent_2=hcat(B*IDO,Eh1f,Eh2f);
     Ent_3=hcat(zeros(BigFloat,s1,s2),Lam*Eh1f,Lam*Eh2f)
-    
+
     AugMat=vcat(Ent_1,Ent_2,Ent_3)
     AugVec=vcat(y,EGGf,zeros(BigFloat,size(Eh1f)))
 
@@ -76,7 +76,6 @@ function CosCont(d,Al,Lam)
     uc=M*(IDO*Res1[1:end-2])
     u_AD=uc[428:428+90]
 
-    return norm(f_AD-y),norm(u_AD[1:10:end])/norm(y[1:10:end])
+    return norm(f_AD-y),norm(u_AD[1:10:end])/norm(y[1:10:end]),Res1[1:end-2]
 
 end
-
