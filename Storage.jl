6ae3b122c20cebb3353872fd66ed0227635aab84
Melanie Vining <melanie.n.vining@gmail.com>
2019-04-30 14:54:53 -0400

    Updated to use storage to generate matrices instead of save as files

--- Storage.jl
+++ Storage.jl
@@ -22,40 +22,24 @@ for i=0:9
   AMat[i+1]=a;
 end
 
-open("FcCos.txt","w") do k
-    writedlm(k,FCMat)
-end
-open("CosAcc.txt","w") do s
-    writedlm(s,AMat)
-end
 
 DOC=DiffOp(D,A,n,F,Al,1);
 IDOC=zeros(BigFloat,size(DOC))
 for i=1:length(DOC[1,:])
     IDOC[i,i]=1/DOC[i,i];
 end
-open("CosDiffOp.txt","w") do l
-    writedlm(l,IDOC)
-end
+
 for i=0:9
   (a,b,c)=SinCont(i)
   FCMatS[:,i+1]=b;
   AMatS[i+1]=a;
 end
-open("FcSin.txt","w") do k
-    writedlm(k,FCMatS)
-end
-open("SinAcc.txt","w") do s
-    writedlm(s,AMatS)
-end
+
 
 DOS=DiffOp(D,A,n,F,Al,0);
 IDOS=zeros(BigFloat,size(DOS))
 for i=1:length(DOS[1,:])
     IDOS[i,i]=1/DOS[i,i];
-end
-open("SinDiffOp.txt","w") do l
-    writedlm(l,IDOS)
 end
 
 (m,C_C,n)=CosMtx(D,A,n,F);
