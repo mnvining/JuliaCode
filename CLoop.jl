b05c3f01fe26e46323ca7259fd2136b534817b16
Melanie Vining <melanie.n.vining@gmail.com>
2019-06-26 14:03:45 -0400

    Changed to take in d instead of 9 to check other polys for al = 1e-3

--- CLoop.jl
+++ CLoop.jl
@@ -1,12 +1,12 @@
 include("NCalc.jl")
-CAM9=zeros(40,1)
-SAM9=zeros(40,1)
-UCM9=zeros(40,1)
-USM9=zeros(40,1)
-LL=collect(BigFloat,-10:29)
+CAM5=zeros(40,1)
+SAM5=zeros(40,1)
+UCM5=zeros(40,1)
+USM5=zeros(40,1)
+LL=logspace(-10,29,40)
 
 
     for j=-10:29
-  (CAM9[j+11],SAM9[j+11],UCM9[j+11],USM9[j+11])=NCalc(9,10 .^LL[j+11],FCMat,FCMatS,C_C,C_S,IDOC,IDOS,m,mm);
+  (CAM5[j+11],SAM5[j+11],UCM5[j+11],USM5[j+11])=NCalc(d,LL[j+11],FCMat,FCMatS,C_C,C_S,IDOC,IDOS,m,mm);
     println(j)
     end
