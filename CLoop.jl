f2376f9a4018bd93191138d7ec6d26851033a80d
Melanie Vining <melanie.n.vining@gmail.com>
2019-06-30 12:52:26 -0400

    More photos for weight comparison

--- CLoop.jl
+++ CLoop.jl
@@ -1,12 +1,12 @@
 include("NCalc.jl")
-CAM5=zeros(40,1)
-SAM5=zeros(40,1)
-UCM5=zeros(40,1)
-USM5=zeros(40,1)
+CAM3=zeros(40,1)
+SAM3=zeros(40,1)
+UCM3=zeros(40,1)
+USM3=zeros(40,1)
 LL=logspace(-10,29,40)
 
 
     for j=-10:29
-  (CAM5[j+11],SAM5[j+11],UCM5[j+11],USM5[j+11])=NCalc(d,LL[j+11],FCMat,FCMatS,C_C,C_S,IDOC,IDOS,m,mm);
+  (CAM3[j+11],SAM3[j+11],UCM3[j+11],USM3[j+11])=NCalc(d,LL[j+11],FCMat,FCMatS,C_C,C_S,IDOC,IDOS,m,mm);
     println(j)
     end
