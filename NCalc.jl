5f92742f74720383c4e74a9749a1e6633c14a608
Melanie Vining <melanie.n.vining@gmail.com>
2019-09-20 11:59:28 -0400

    NCalc has tol now too

--- NCalc.jl
+++ NCalc.jl
@@ -2,11 +2,11 @@ using SymPy
 using LinearAlgebra
 using GenericSVD
 
-function NCalc(d,w,FCMatC,FCMatS,C_C,C_S,IDOC,IDOS,m,mm)
+function NCalc(d,w,FCMatC,FCMatS,C_C,C_S,IDOC,IDOS,m,mm,tol)
   include("MP.jl")
   include("All.jl")
   include("Spaces.jl")
-  (yc,ys,Gc,Gs)=MP(d,w,FCMatC,FCMatS,C_C,C_S,IDOC,IDOS)
+  (yc,ys,Gc,Gs)=MP(d,w,FCMatC,FCMatS,C_C,C_S,IDOC,IDOS,tol)
   setprecision(230);
   D=BigFloat(350)/BigFloat(100);
   A=BigFloat(125)/BigFloat(100);
