fe243b13d6b3ab404ff6eac0f1b321894310c0be
Melanie Vining <melanie.n.vining@gmail.com>
2019-12-04 18:27:24 -0500

    more

--- SaveThis.jl
+++ SaveThis.jl
@@ -11,9 +11,9 @@ for j=1:171
     global PP=zeros(1,4);
     global MaxCount=Inf;
     for i=1:121
-        (CS,SS,CA,SA,MC,MS)=AllModule.TotalCalc(d,w[i],1e-7,AllModule.FCMat,AllModule.FCMatS,AllModule.C_C,AllModule.C_S,IDOC,IDOS,AllModule.m,AllModule.mm);
+        (CS,SS,CA,SA,MC,MS)=AllModule.TotalCalc(d,w[i],1e-18,AllModule.FCMat,AllModule.FCMatS,AllModule.C_C,AllModule.C_S,IDOC,IDOS,AllModule.m,AllModule.mm);
         if 1-CS>1e-18
-            if MC<1e7
+            if MC<1e11
                 IL=hcat(CS,CA,MC,i);
                 global PP=vcat(PP,IL);
             end
